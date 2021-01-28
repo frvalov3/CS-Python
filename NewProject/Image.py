24e546ff6e38a5b783c20a5818c1a689d8ea36ca
Loi Tran <frvalov3@gmail.com>
2021-01-28 18:58:03 +0700

    new project

---
+++ NewProject/Image.py
@@ -0,0 +1,9 @@
+# importing matplotlib modules 
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt 
+  
+# Read Images 
+img = mpimg.imread('Imag.png') 
+  
+# Output Images 
+plt.imshow(img) 
