06cd185d72e82bb0beb0e1343a5e9292d617f446
Loi Tran <frvalov3@gmail.com>
2021-04-21 15:33:13 +0700

    add file

---
+++ PetPracticeProject1/Vuejs/Vue.vue
@@ -0,0 +1,12 @@
+Vue.component('blog-post', {
+  render: function (createElement) {
+    var header = this.$slots.header
+    var body   = this.$slots.default
+    var footer = this.$slots.footer
+    return createElement('div', [
+      createElement('header', header),
+      createElement('main', body),
+      createElement('footer', footer)
+    ])
+  }
+})
